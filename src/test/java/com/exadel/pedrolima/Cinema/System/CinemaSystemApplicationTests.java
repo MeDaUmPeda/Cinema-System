2026243a8fd4742df3a74c396f957c813b52f1c2
LordeP26 <ped.li2608@gmail.com>
2025-09-10 12:29:06 -0300

    Initial project

---
+++ src/test/java/com/exadel/pedrolima/Cinema/System/CinemaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.pedrolima.Cinema.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
